ae0458c0821bd44de8b533022ddb577e14645682
yoonmikim <yoonlemon20@gmail.com>
2025-04-18 14:38:48 +0900

    수정

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { createRequire } from 'node:module';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
-import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 
 export default defineConfig(({ mode }) => {
-    const _buildOptions = (mode === 'prod') ? {
+    const _buildOptions = (mode === 'production') ? {
         sourcemap: true,
         minify: 'terser',
         terserOptions: {
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
     } : {};
 
     return {
-        base: '/health/',
+        base: '/health/',  // GitHub Pages base URL
         plugins: [
             vue({
                 template: {
