179ea0db0d99674d5e8defa59ba304a026c71f1a
yoonmikim <yoonlemon20@gmail.com>
2025-04-18 14:31:40 +0900

    config수정

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
-import { createRequire } from 'node:module';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
+import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 
 export default defineConfig(({ mode }) => {
-    const _buildOptions = (mode === 'production') ? {
+    const _buildOptions = (mode === 'prod') ? {
         sourcemap: true,
         minify: 'terser',
         terserOptions: {
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
     } : {};
 
     return {
-        base: process.env.NODE_ENV === 'production' ? '/health/' : '/',
+        base: '/health/',
         plugins: [
             vue({
                 template: {
